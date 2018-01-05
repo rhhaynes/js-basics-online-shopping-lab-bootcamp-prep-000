88e41472a6d91db8b39d1b53bc86554753d747c8
Randall Harris Haynes <harrishaynes@gmail.com>
2018-01-05 18:27:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function total() {
 function removeFromCart(item) {
   var itemNotFound = 1;
   for (let i=0; i<cart.length; i++){
-    if (item===Object.keys(cart[i])){
+    if (Object.keys(cart[i])){
       cart.splice(i,1);
       itemNotFound = 0;
       break;
